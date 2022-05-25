f53a7efa6d8fae24ffa2b721467293b5bc657413
Rafa Lozano <rafael@avena.io>
2022-05-25 11:36:53 -0500

    fix: hot fix avoid download pdf in mobile

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getWeatherFrom } from '../../services/wheater';
 import { Drawer, Button } from 'antd';
+import Head from 'next/head';
 const Home = () => {
   const [wheaterRawData, setWheaterRawData] = useState(null);
   const [drawerVisible, setDrawerVisible] = useState(false);
@@ -16,6 +17,9 @@ const Home = () => {
   }, []);
   return (
     <div>
+      <Head>
+        <title>{`Wheater ${wheaterRawData?.location?.name}`}</title>
+      </Head>
       <h1>{wheaterRawData?.location?.name}</h1>
       {unit === 'celsius' ? (
         <h2>{`${wheaterRawData?.current?.temp_c} °C`}</h2>
@@ -23,7 +27,18 @@ const Home = () => {
         <h2>{`${wheaterRawData?.current?.temp_f} °F`}</h2>
       )}
       <Button onClick={() => setDrawerVisible(true)}>Ver</Button>
-      <Drawer visible={drawerVisible} onClose={() => setDrawerVisible(false)}>
+      <Drawer
+        visible={drawerVisible}
+        onClose={() => setDrawerVisible(false)}
+        title={null}
+        placement="bottom"
+        contentWrapperStyle={{
+          borderTopLeftRadius: '20px',
+          borderTopRightRadius: '20px',
+          overflow: 'hidden'
+        }}
+        height={'420px'}
+      >
         <h1>{wheaterRawData?.location?.name}</h1>
       </Drawer>
     </div>
